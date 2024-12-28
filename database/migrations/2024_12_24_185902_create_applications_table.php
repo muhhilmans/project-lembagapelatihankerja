a33abf4016589d729e271f0277866b05447a9b26
Muh Hilman Sholehudin <muhhilmansholehudin@gmail.com>
2024-12-28 10:55:03 +0700

    fixing

--- database/migrations/2024_12_24_185902_create_applications_table.php
+++ database/migrations/2024_12_24_185902_create_applications_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignUuid('servant_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreignUuid('vacancy_id')->nullable()->references('id')->on('vacancies')->onDelete('cascade');
             $table->foreignUuid('employe_id')->nullable()->references('id')->on('users')->onDelete('cascade');
-            $table->enum('status', ['pending', 'interview', 'passed', 'choose', 'verify', 'accepted', 'rejected'])->default('pending');
+            $table->enum('status', ['pending', 'interview', 'passed', 'choose', 'verify', 'contract', 'accepted', 'rejected'])->default('pending');
             $table->text('notes_interview')->nullable();
             $table->text('notes_verify')->nullable();
             $table->text('notes_accepted')->nullable();
