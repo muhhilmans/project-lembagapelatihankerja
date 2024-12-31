05891735267d139cb304454007500743f880fcba
Muh Hilman Sholehudin <muhhilmansholehudin@gmail.com>
2024-12-31 10:40:59 +0700

    <add> [Landing]: Template Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,13 +23,13 @@
                                     <input type="password" class="form-control form-control-user" id="password"
                                         name="password" placeholder="Password" required>
                                 </div>
-                                <div class="form-group">
+                                {{-- <div class="form-group">
                                     <div class="custom-control custom-checkbox small">
                                         <input type="checkbox" class="custom-control-input" id="customCheck">
                                         <label class="custom-control-label" for="customCheck">Remember
                                             Me</label>
                                     </div>
-                                </div>
+                                </div> --}}
                                 <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
                             </form>
                             <hr>
