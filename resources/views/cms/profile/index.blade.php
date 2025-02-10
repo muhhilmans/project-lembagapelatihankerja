7e3a289b87996025c91fbb7e4afd674c1608372a
Muh Hilman Sholehudin <muhhilmansholehudin@gmail.com>
2025-02-11 06:15:57 +0700

    <update> [Profile]: Add Status Account

--- resources/views/cms/profile/index.blade.php
+++ resources/views/cms/profile/index.blade.php
@@ -3,7 +3,10 @@
 @section('content')
     <!-- Page Heading -->
     <div class="d-flex justify-content-between align-items-baseline">
-        <h1 class="h3 mb-4 text-gray-800">Profile Saya</h1>
+        <div class="d-flex flex-row">
+            <h1 class="h3 mb-4 text-gray-800">Profile Saya</h1>
+            <sup class="ml-2"><span class="badge bg-{{ $data->is_active ? 'success' : 'warning' }} text-{{ $data->is_active ? 'light' : 'dark' }} p-2"><strong>{{ $data->is_active ? 'Terverifikasi' : 'Belum Diverifikasi' }}</strong></span></sup>
+        </div>
         <a href="{{ route('profile.edit', $data->id) }}" class="btn btn-warning mr-1"><i
                 class="fas fa-fw fa-user-edit"></i></a>
     </div>
