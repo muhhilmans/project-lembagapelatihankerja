8a224c8c91a2b6bb60c6107d52c72a285b4356bd
Muh Hilman Sholehudin <muhhilmansholehudin@gmail.com>
2025-03-27 12:54:38 +0700

    <add> [Library]: PWA Library

--- resources/views/auth/layout/main.blade.php
+++ resources/views/auth/layout/main.blade.php
@@ -8,6 +8,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <!-- PWA  -->
+    <meta name="theme-color" content="#6777ef" />
+    <link rel="apple-touch-icon" href="{{ asset('assets/img/logo.png') }}">
+    <link rel="manifest" href="{{ asset('/manifest.json') }}">
 
     <title>{{ $title }} | {{ env('APP_NAME') }}</title>
 
@@ -44,6 +48,24 @@
 
     <!-- Custom scripts for all pages-->
     <script src="{{ asset('assets/js/sb-admin-2.min.js') }}"></script>
+
+    <script src="{{ asset('/sw.js') }}"></script>
+    <script>
+        if ("serviceWorker" in navigator) {
+            // Register a service worker hosted at the root of the
+            // site using the default scope.
+            navigator.serviceWorker.register("/sw.js").then(
+                (registration) => {
+                    console.log("Service worker registration succeeded:", registration);
+                },
+                (error) => {
+                    console.error(`Service worker registration failed: ${error}`);
+                },
+            );
+        } else {
+            console.error("Service workers are not supported.");
+        }
+    </script>
 
     @stack('custom-script')
 </body>
